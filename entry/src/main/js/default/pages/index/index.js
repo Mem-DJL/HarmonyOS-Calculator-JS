5195ca461aa82cf4564b22641eaf426a13dc2c7b
DuJinLong <2540187660@qq.com>
2021-10-28 21:20:30 +0800

    optimize the layout

--- entry/src/main/js/default/pages/index/index.js
+++ entry/src/main/js/default/pages/index/index.js
@@ -1,9 +1,15 @@
 export default {
     data: {
         expression: "1+1",
-        result: "= 2"
+        result: "= 2",
+        temp_result: 0,
+        memory_data: 0,
     },
     onInit() {
 
+    },
+    input(e) {
+        var data = e.target.attr.value;
+        console.log(data);
     }
 }
